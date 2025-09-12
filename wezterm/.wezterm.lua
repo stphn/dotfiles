759201f93d3bc21a02cdcce36cc0c041a9857a93
stphn <sgoeuriot@gmail.com>
2025-09-12 07:50:53 +0200

    moved wezterm inside its folder

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,27 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+config.font = wezterm.font("JetBrains Mono", { weight = "Regular" })
+config.font_size = 18
+-- Cursor styles
+-- config.default_cursor_style = "BlinkingBlock" -- solid block that blinks
+-- config.default_cursor_style = "SteadyBlock" -- solid block, no blink
+-- config.default_cursor_style = "BlinkingBar"  -- thin bar, blinks
+config.default_cursor_style = "SteadyBar" -- thin bar, no blink
+-- config.default_cursor_style = "BlinkingUnderline"
+-- config.default_cursor_style = "SteadyUnderline"
+
+config.enable_tab_bar = false
+
+config.window_decorations = "RESIZE"
+
+-- 🎨 Theme
+config.color_scheme = "Nord (Gogh)"
+
+-- and finally, return the configuration to wezterm
+return config
